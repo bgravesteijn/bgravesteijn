dc1b1d9b1260523899a9683fb746b281585ddd51
bgravesteijn <b.gravesteijn@erasmusmc.nl>
2018-05-16 14:33:41 +0200

    updated catterpillar() --> better workability

--- R/Catterpillar.R
+++ R/Catterpillar.R
@@ -4,19 +4,18 @@
 #'
 #' @param x Alist of fitted models (for multiple imputed dataset fitted models)
 #' @param fitter A character indicating random effects fitting formula
-#' @param grp.var = character vector for grouping variable
 #' @param grp.var.t = character indicating type of grouping variable, e.g.: "country"
 #' @param MOR = logical indicating whether the median odds ratio should be printed, default is TRUE
 #' @return Returns a plot containing mean and 95% CI, with median odds ratio
-catterpillar<-function(x = NULL, fitter= NULL,grp.var=NULL, grp.var.t = NULL, MOR=TRUE){
+catterpillar<-function(x = NULL, fitter= NULL, grp.var.t = NULL, MOR=TRUE){
   #x = list of fitted models (for multiple imputed dataset fitted models)
   #fitter = character indicating random effects fitting formula
-  #grp.var = character vector for grouping variable
   #grp.var.t = character indicating type of grouping variable, e.g.: "country"
   #MOR = logical indicating whether the median odds ratio should be printed, default is TRUE
 
   library(lme4)
   library(ordinal)
+  library(ggplot2)
 
   MORcalc <- function(my.var, digits = 2)
   { # MOR arguments: my.var = variance associated with level 2 clustering variable
@@ -26,11 +25,8 @@ catterpillar<-function(x = NULL, fitter= NULL,grp.var=NULL, grp.var.t = NULL, MO
     return(Median.OR)}
 
   length_list<-length(x)
-  plot.df<-data.table(data.frame(grp=grp.var,
-                                 condval=NA,
-                                 sd=NA,
-                                 lo=NA,
-                                 hi=NA))
+  length_grp<-length(data.frame(ranef(x, which=grp.var.t))$grp)
+
   #grp = vector of the grouping variable, e.g. countries/schools...
   #re = vector of the random effects for each value of the grp variable
   #condvar = vector of the conditional variances of the random effects of the grouping variable
@@ -39,45 +35,45 @@ catterpillar<-function(x = NULL, fitter= NULL,grp.var=NULL, grp.var.t = NULL, MO
 
   if (fitter=="glmer"){
     if(length_list==1){
-      plot.df$condval<-data.frame(lme4::ranef(x, which=grp.var.t))$condval
-      plot.df$sd<-data.frame(lme4::ranef(x, which=grp.var.t, condVar=TRUE))$condsd
-      plot.df$lo<-plot.df$condval-1.96*plot.df$sd
-      plot.df$hi<-plot.df$condval+1.96*plot.df$sd
+      plot.df<-data.frame(lme4::ranef(x, which=grp.var.t, condVar=TRUE))
+      plot.df$lo<-plot.df$condval-1.96*plot.df$condsd
+      plot.df$hi<-plot.df$condval+1.96*plot.df$condsd
       MOR<-MORcalc(data.frame(lme4::VarCorr(x))$sdcor^2)
     }else{
-      condval<-matrix(data = rep(NA, length(grp.var)*length_list), ncol = length_list)
-      sd<-matrix(data = rep(NA, length(grp.var)*length_list), ncol = length_list)
+      condval<-matrix(data = rep(NA, length_grp*length_list), ncol = length_list)
+      sd<-matrix(data = rep(NA, length_grp*length_list), ncol = length_list)
       for (i in 1:length_list){
         condval[,i]<-data.frame(lme4::ranef(x[[i]], which=grp.var.t, condVar=TRUE))$condval
         sd[,i]<-data.frame(lme4::ranef(x[[i]], which=grp.var.t, condVar=TRUE))$condsd
         MOR[i]<-MORcalc(data.frame(lme4::VarCorr(x[[i]]))$sdcor^2)
       }
+      plot.df<-data.frame(lme4::ranef(x[[1]], which=grp.var.t, condvar=TRUE))
       plot.df$condval<-apply(condval, 1, mean)
-      plot.df$sd<-apply(sd, 1, mean)
-      plot.df$lo<-plot.df$condval-1.96*plot.df$sd
-      plot.df$hi<-plot.df$condval+1.96*plot.df$sd
+      plot.df$condsd<-apply(sd, 1, mean)
+      plot.df$lo<-plot.df$condval-1.96*plot.df$condsd
+      plot.df$hi<-plot.df$condval+1.96*plot.df$condsd
       MOR<-mean(MOR)
     }
   }else{
     if(fitter%in%c("clmm2", "clmm")){
       if(length_list==1){
-        plot.df$condval<-data.frame(ordinal::ranef(x, which=grp.var.t))$condval
-        plot.df$sd<-data.frame(ordinal::ranef(x, which=grp.var.t, condVar=TRUE))$condsd
-        plot.df$lo<-plot.df$condval-1.96*plot.df$sd
-        plot.df$hi<-plot.df$condval+1.96*plot.df$sd
+        plot.df<-data.frame(ordinal::ranef(x, which=grp.var.t, condVar=TRUE))
+        plot.df$lo<-plot.df$condval-1.96*plot.df$condsd
+        plot.df$hi<-plot.df$condval+1.96*plot.df$condsd
         MOR<-MORcalc(data.frame(ordinal::VarCorr(x))$sdcor^2)
       }else{
-        condval<-matrix(data = rep(NA, length(grp.var)*length_list), ncol = length_list)
-        sd<-matrix(data = rep(NA, length(grp.var)*length_list), ncol = length_list)
+        condval<-matrix(data = rep(NA, length_grp*length_list), ncol = length_list)
+        sd<-matrix(data = rep(NA, length_grp*length_list), ncol = length_list)
         for (i in 1:ncol(condval)){
           condval[,i]<-data.frame(ordinal::ranef(x[[i]], which=grp.var.t, condVar=TRUE))$condval
           sd[,i]<-data.frame(ordinal::ranef(x[[i]], which=grp.var.t, condVar=TRUE))$condsd
           MOR[i]<-MORcalc(data.frame(ordinal::VarCorr(x[[i]]))$sdcor^2)
         }
+        plot.df<-ordinal::ranef(x[[1]], which=grp.var.t, condvar=TRUE)
         plot.df$condval<-apply(condval, 1, mean)
-        plot.df$sd<-apply(sd, 1, mean)
-        plot.df$lo<-plot.df$condval-1.96*plot.df$sd
-        plot.df$hi<-plot.df$condval+1.96*plot.df$sd
+        plot.df$condsd<-apply(sd, 1, mean)
+        plot.df$lo<-plot.df$condval-1.96*plot.df$condsd
+        plot.df$hi<-plot.df$condval+1.96*plot.df$condsd
         MOR<-mean(MOR)
       }
     }else{
