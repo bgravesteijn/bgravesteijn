5f5235782174df452f5a94d4e3ef4adf02eb3ce2
bgravesteijn <b.gravesteijn@erasmusmc.nl>
2018-05-16 15:25:06 +0200

    update description and add ROC plot function

---
+++ R/ROC plot.R
@@ -0,0 +1,15 @@
+#' Make a ROC plot, with AUC
+#'
+#' @param x Alist of fitted models (for multiple imputed dataset fitted models)
+#' @param obs.v A vector of the observed outcome variable: (0,1)
+#' @return Returns a ROC plot containing AUC
+roc.model<-function(x, obs.v){
+  library(ROCR)
+  obs.df<-data.frame(obs.v)
+  obs.df$pred<-predict(x, type="response")
+  pred<-prediction(obs.df$pred, obs.df$obs.v)
+  perf<-performance(pred, measure = "tpr", "fpr")
+  auc<-round(performance(pred, measure="auc")@y.values[[1]],2)
+  plot(perf)
+  text(x=0.75, y=0.25 ,paste("AUC =", auc))
+}
