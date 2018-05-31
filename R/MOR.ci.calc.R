9469eb89de16feb3406cd3a0512c21bc7ceb769b
bgravesteijn <b.gravesteijn@erasmusmc.nl>
2018-05-31 16:03:49 +0200

    enable subsetting

--- R/MOR.ci.calc.R
+++ R/MOR.ci.calc.R
@@ -4,11 +4,12 @@
 #' @param data a (list of) data.frame(s) or data.table(s), or a MIDS object
 #'  containing the data on which the model was fitted
 #' @param grp.var.t the grouping variable, e.g.: "country"
-MOR.ci.calc <- function (formula=NULL, data=NULL, grp.var.t=NULL){
+#' @param set.subset a logical vector, indicating whether a subject is included or not in the analysis
+MOR.ci.calc <- function (formula=NULL, data=NULL, grp.var.t=NULL, set.subset=NULL){
 
    fit.mor<-function(x, indices){
     fml<-as.formula(formula)
-    fit<-lme4::glmer(formula=fml, data=x[indices,], family="binomial")
+    fit<-lme4::glmer(formula=fml, data=x[indices,], subset=set.subset, family="binomial")
     var<-data.table::data.table(data.frame(lme4::VarCorr(fit)))[grp==grp.var.t,]$vcov
     mor<-bgravesteijn::MORcalc(my.var = var)
     return(mor)
